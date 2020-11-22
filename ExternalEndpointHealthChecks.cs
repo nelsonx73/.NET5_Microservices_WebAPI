74dc9f99e783087dc355fe95de68101786c4ec6b
Marmolejos <NelsonM@sandhillscenter.org>
2020-11-21 22:07:26 -0500

    App

---
+++ ExternalEndpointHealthChecks.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloDotnet5
+{
+    public class ExternalEndpointHealthChecks : IHealthCheck
+    {
+        private readonly ServiceSettings _settings;
+
+        public ExternalEndpointHealthChecks(IOptions<ServiceSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            Ping ping = new();
+            var reply = await ping.SendPingAsync(_settings.OpenWeatherHost);
+            if (reply.Status != IPStatus.Success)
+            {
+                return HealthCheckResult.Unhealthy();
+            }
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
---
+++ WeatherClient.cs
@@ -0,0 +1,31 @@
+using HelloDotnet5;
+using Microsoft.Extensions.Options;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace HelloDotnet5
+{
+    public class WeatherClient {
+        private readonly HttpClient _httpClient;
+        private readonly ServiceSettings _settings;
+
+        public WeatherClient(HttpClient httpClient, IOptions<ServiceSettings> options){
+            _httpClient = httpClient;
+            _settings = options.Value;
+        }
+
+        public record Weather(string description);
+        public record Main(decimal temp);
+        public record Forecast(Weather[] weather, Main main, long dt);
+
+        public async Task<Forecast> GetCurrentWeatherAsync(string city)
+        {
+            var forecast = await _httpClient
+                .GetFromJsonAsync<Forecast>($"https://{_settings.OpenWeatherHost}/data/2.5/weather?q={city}&appid={_settings.ApiKey}&units=metric");
+            return forecast;
+        }
+
+    }
+
+}
